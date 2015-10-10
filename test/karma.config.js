c81cda143a6eb7bb02635338827720ba17d7ce75
jxnblk <jacksonblack@gmail.com>
2015-10-10 19:01:34 -0400

    Adjust travis karma config

--- test/karma.config.js
+++ test/karma.config.js
@@ -1,94 +1,101 @@
 
-module.exports = function (config) {
-  config.set({
-    browsers: [
-      'Firefox',
-      'mobile',
-      'tablet',
-      'desktop',
-      'Safari'
-    ],
+var configuration = {
+  browsers: [
+    'Firefox',
+    'mobile',
+    'tablet',
+    'desktop',
+    'Safari'
+  ],
 
-    customLaunchers: {
-      mobile: {
-        base: 'Chrome',
-        flags: [ '--window-size=320,640' ]
-      },
-      tablet: {
-        base: 'Chrome',
-        flags: [ '--window-size=640,480' ]
-      },
-      desktop: {
-        base: 'Chrome',
-        flags: [ '--window-size=1280,960' ]
-      },
-      travisChrome: {
-        base: 'Chrome',
-        flags: [
-          '--window-size=1280,960',
-          '--no-sandbox'
-        ]
-      }
+  customLaunchers: {
+    mobile: {
+      base: 'Chrome',
+      flags: [ '--window-size=320,640' ]
     },
-
-    autoWatch: true,
-    // singleRun: true,
-
-    files: [
-      'index.js',
-    ],
-
-    frameworks: [
-      'chai',
-      'mocha'
-    ],
-
-    plugins: [
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-safari-launcher',
-      'karma-chai',
-      'karma-mocha',
-      'karma-mocha-reporter',
-      'karma-webpack'
-    ],
-
-    preprocessors: {
-      'index.js': [ 'webpack' ]
+    tablet: {
+      base: 'Chrome',
+      flags: [ '--window-size=640,480' ]
     },
-
-    reporters: [ 'mocha' ],
-
-    webpack: {
-      resolve: {
-        extensions: ['', '.js', '.jsx']
-      },
-      module: {
-        loaders: [
-          {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-          },
-          {
-            test: /\.json$/,
-            loader: 'json-loader'
-          }
-        ]
-      }
+    desktop: {
+      base: 'Chrome',
+      flags: [ '--window-size=1280,960' ]
     },
-
-    webpackMiddleware: {
-      noInfo: true
-    },
-
-    client: {
-      mocha: {
-        reporter: 'html',
-        ui: 'bdd'
-      }
+    travisChrome: {
+      base: 'Chrome',
+      flags: [
+        '--window-size=1280,960',
+        '--no-sandbox'
+      ]
     }
+  },
 
-  })
+  autoWatch: true,
+  // singleRun: true,
+
+  files: [
+    'index.js',
+  ],
+
+  frameworks: [
+    'chai',
+    'mocha'
+  ],
+
+  plugins: [
+    'karma-chrome-launcher',
+    'karma-firefox-launcher',
+    'karma-safari-launcher',
+    'karma-chai',
+    'karma-mocha',
+    'karma-mocha-reporter',
+    'karma-webpack'
+  ],
+
+  preprocessors: {
+    'index.js': [ 'webpack' ]
+  },
+
+  reporters: [ 'mocha' ],
+
+  webpack: {
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.json$/,
+          loader: 'json-loader'
+        }
+      ]
+    }
+  },
+
+  webpackMiddleware: {
+    noInfo: true
+  },
+
+  client: {
+    mocha: {
+      reporter: 'html',
+      ui: 'bdd'
+    }
+  }
+
 }
 
+if (process.env.TRAVIS) {
+  configuration.singleRun = true
+  configuration.browsers = ['travisChrome', 'Firefox'];
+}
+
+
+module.exports = function (config) {
+  config.set(configuration)
+}
