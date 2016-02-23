7ebbf99ea3b0583d99c862972fc39daa36b5d303
jxnblk <jacksonblack@gmail.com>
2016-02-23 14:12:28 -0500

    Adjust Box breakpoint props

--- src/Box.js
+++ src/Box.js
@@ -2,6 +2,9 @@
 import React from 'react'
 import styles from './styles'
 
+/**
+ * Sets padding and width and works independently or as a child of <Flex />.
+ */
 
 const Box = ({
   p,
@@ -14,9 +17,6 @@ const Box = ({
   flex,
   auto,
   col,
-  sm,
-  md,
-  lg,
   style,
   className,
   ...props
@@ -31,16 +31,11 @@ const Box = ({
   let width = w(col)
 
   if (typeof window !== 'undefined') {
-    width = w(col)
-    if (window.matchMedia(breakpoints.sm).matches) {
-      width = w(sm) || width
-    }
-    if (window.matchMedia(breakpoints.md).matches) {
-      width = w(md) || width
-    }
-    if (window.matchMedia(breakpoints.lg).matches) {
-      width = w(lg) || width
-    }
+    Object.keys(breakpoints).forEach(key => {
+      if (props[key] && window.matchMedia(breakpoints[key]).matches) {
+        width = w(props[key]) || width
+      }
+    })
   }
 
   const sx = Object.assign({}, style, {
@@ -52,31 +47,38 @@ const Box = ({
     paddingBottom: py ? scale[py] : (pb ? scale[pb] : null),
     paddingLeft:   px ? scale[px] : (pl ? scale[pl] : null),
     paddingRight:  px ? scale[px] : (pr ? scale[pr] : null),
-    width: width,
+    width,
     flexBasis: width
   })
+
+  const cx = className ? `Box ${className}` : 'Box'
 
   return <div
     {...props}
     style={sx}
-    className={className ? `Box ${className}` : 'Box'} />
+    className={cx} />
 }
 
 Box.propTypes = {
-  style: React.PropTypes.object,
-  className: React.PropTypes.string,
+  /** Sets flex: 1 1 auto */
   auto: React.PropTypes.bool,
+  /** Sets display: flex */
   flex: React.PropTypes.bool,
+  /** Sets width and flex-basis based on a 12 column grid */
   col: React.PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
-  sm: React.PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
-  md: React.PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
-  lg: React.PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+  /** Sets padding based on a scale of 0–4 */
   p: React.PropTypes.oneOf([0, 1, 2, 3, 4]),
+  /** Sets padding-left and padding-right based on a scale of 0–4 */
   px: React.PropTypes.oneOf([0, 1, 2, 3, 4]),
+  /** Sets padding-top and padding-bottom based on a scale of 0–4 */
   py: React.PropTypes.oneOf([0, 1, 2, 3, 4]),
+  /** Sets padding-top based on a scale of 0–4 */
   pt: React.PropTypes.oneOf([0, 1, 2, 3, 4]),
+  /** Sets padding-bottom based on a scale of 0–4 */
   pb: React.PropTypes.oneOf([0, 1, 2, 3, 4]),
+  /** Sets padding-left based on a scale of 0–4 */
   pl: React.PropTypes.oneOf([0, 1, 2, 3, 4]),
+  /** Sets padding-right based on a scale of 0–4 */
   pr: React.PropTypes.oneOf([0, 1, 2, 3, 4]),
 }
 
