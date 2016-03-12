4be0840602a0b3f21b4ae3276755f6d14f75ef34
jxnblk <jacksonblack@gmail.com>
2016-03-12 09:41:08 -0500

    Replace chai with expect

--- test/config.spec.js
+++ test/config.spec.js
@@ -1,29 +1,29 @@
 
-import { expect } from 'chai'
+import expect from 'expect'
 import config, { scale, breakpoints } from '../src/config'
 
 describe('config', () => {
   it('should be an object', () => {
-    expect(config).to.be.an('object')
+    expect(config).toBeAn('object')
   })
 
   it('should have a scale array', () => {
-    expect(config.scale).to.be.an('array')
+    expect(config.scale).toBeAn('array')
   })
 
   it('should export scale', () => {
-    expect(scale).to.be.an('array')
+    expect(scale).toBeAn('array')
   })
 
   it('should have a breakpoints array', () => {
-    expect(config.breakpoints).to.be.an('object')
+    expect(config.breakpoints).toBeAn('object')
   })
 
   it('should export breakpoints', () => {
-    expect(breakpoints).to.be.an('object')
+    expect(breakpoints).toBeAn('object')
   })
 
   it('should have default breakpoints', () => {
-    expect(config.breakpoints.sm).to.be.a('string')
+    expect(config.breakpoints.sm).toBeA('string')
   })
 })
