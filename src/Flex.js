cd510d82ea90de607823490ac4868ae035fc5503
jxnblk <jacksonblack@gmail.com>
2016-07-30 21:49:25 -0400

    Fix linting errors

--- src/Flex.js
+++ src/Flex.js
@@ -11,11 +11,11 @@ const Flex = ({ className, ...props }) => {
 }
 
 const responsivePropCheck = (props, propName, componentName) => {
-    if (typeof props[propName] === 'boolean') {
-      return new Error (
-        `Warning! The \`${propName}\` prop supplied to \`${componentName}\` now sets width instead of setting display flex. See https://github.com/jxnblk/reflexbox`
-      )
-    }
+  if (typeof props[propName] === 'boolean') {
+    return new Error(
+      `Warning! The \`${propName}\` prop supplied to \`${componentName}\` now sets width instead of setting display flex. See https://github.com/jxnblk/reflexbox`
+    )
+  }
 }
 
 Flex.propTypes = {
