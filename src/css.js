0694cf943d5ee119d84dcfe86b98c92d78cf2e2e
Brent Jackson <jxnblk@gmail.com>
2017-06-04 16:05:43 -0400

    Prototype atomic

--- src/css.js
+++ src/css.js
@@ -1,17 +1,114 @@
-const toCSS = config => (selector, rules) => {
-  const rulesets = []
-  const breaks = [ null, ...config.breakpoints ]
-  breaks.forEach((b, i) => {
-    if (!rules[i] || !rules[i].length) return
-    const body = rules[i].map(dec).join(';')
-    const ruleset = selector + '{' + body + '}'
-    rulesets.push(media(b, ruleset))
-  })
+import sheet from './sheet'
+import {
+  toArr,
+  num
+} from './util'
 
-  return rulesets
+const REG = /^([wmp][trblxy]?|flex|wrap|column|align|justify)$/
+const cache = {}
+
+const css = config => props => {
+  const next = {}
+  const classNames = []
+
+  const breaks = [ null, ...config.breakpoints ]
+
+  for (let key in props) {
+    const val = props[key]
+    if (!REG.test(key)) {
+      next[key] = val
+      continue
+    }
+    const id = '_Rfx' + sheet.cssRules.length.toString(36)
+    const k = key.charAt(0)
+    const style = stylers[key] || stylers[k]
+    const rules = toArr(val).map((v, i) => {
+      const bp = breaks[i]
+      const decs = style(key, v)
+      const cn = id + '_' + (bp || '')
+      const body = `.${cn}{${decs}}`
+      const rule = media(bp, body)
+      const _key = (bp || '') + decs
+
+      if (cache[_key]) {
+        classNames.push(cache[_key])
+        return null
+      } else {
+        classNames.push(cn)
+        cache[_key] = cn
+        return rule
+      }
+    }).filter(r => r !== null)
+    sheet.insert(rules)
+  }
+
+  return [ next, classNames.join(' ') ]
 }
 
+/* todo
+const createRules = (breaks, style) => (v, i) => {
+      const bp = breaks[i]
+      const decs = style(key, v)
+      const cn = id + '_' + (bp || '')
+      const body = `.${cn}{${decs}}`
+      const rule = media(bp, body)
+
+      const _key = (bp || '') + decs
+      if (cache[_key]) {
+        classNames.push(cache[_key])
+        return null
+      } else {
+        classNames.push(cn)
+        cache[_key] = cn
+        return rule
+      }
+}
+
+const createClassNames = rules => {}
+*/
+
 const dec = args => args.join(':')
+const rule = args => args.join(';')
 const media = (bp, body) => bp ? `@media screen and (min-width:${bp}em){${body}}` : body
 
-export default toCSS
+const width = (key, n) => dec([ 'width', !num(n) || n > 1 ? n : (n * 100) + '%' ])
+
+const space = (key, n) => {
+  const [ a, b ] = key.split('')
+  const prop = a === 'm' ? 'margin' : 'padding'
+  const dirs = directions[b] || ['']
+  const neg = n < 0 ? -1 : 1
+  const val = !num(n) ? n : (scale[n] || n) * neg + 'px'
+  return rule(dirs.map(d => dec([ prop + d, val ])))
+}
+
+const scale = [ 0, 8, 16, 32, 64 ]
+const directions = {
+  t: [ '-top' ],
+  r: [ '-right' ],
+  b: [ '-bottom' ],
+  l: [ '-left' ],
+  x: [ '-left', '-right' ],
+  y: [ '-top', '-bottom' ],
+}
+
+const flex = (key, n) => dec([ 'display', 'flex' ])
+const wrap = (key, n) => dec([ 'flex-wrap', 'wrap' ])
+const column = (key, n) => dec([ 'flex-direction', 'column' ])
+const align = (key, n) => dec([ 'align-items', n ])
+const justify = (key, n) => dec([ 'justify-content', n ])
+const order = (key, n) => dec([ 'order', n ])
+
+const stylers = {
+  w: width,
+  m: space,
+  p: space,
+  flex,
+  wrap,
+  column,
+  align,
+  justify,
+  order
+}
+
+export default css
--- src/parse.js
+++ src/parse.js
@@ -1,23 +1,8 @@
-import sheet from './sheet'
-import space from './space'
-import flex from './flex-props'
 import css from './css'
 
 const parse = config => _props => {
-  const id = sheet.cssRules.length.toString(36)
-  const cn = '_reflex' + id
-  const sl = '.' + cn
-
-  const [ a, styles ] = space(_props)
-  const [ next, flexRules ] = flex(a)
-
-  const rules = css(config)(sl, styles)
-  const rulesB = css(config)(sl, flexRules)
-
-  sheet.insert(rules)
-  sheet.insert(rulesB)
-
-  next.className = cn
+  const [ next, className ] = css(config)(_props)
+  next.className = className
 
   return next
 }
